05a9b42db4ad5f20b778b439a179cc142a642670
andres <andres.guevara03@uceva.edu.co>
2025-03-12 09:46:01 -0500

    Hola mundo Spring Boot

---
+++ HolaMundo/src/main/java/co/edu/uceva/holamundo/HolaMundoController.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.holamundo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+    @GetMapping("/hola/{nombre}")
+    public String hola(@PathVariable("nombre") String nombre) {
+        return "Hola " + nombre + " desde Spring Boot!";
+    }
+}
